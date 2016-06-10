475a59c731eae8e8085e68e01d678904337ca327
Marius Ileana <visvadw@gmail.com>
2016-06-10 16:43:03 +0300

    JavaScript ES6 and ES7 notes

---
+++ JS_ES_6-7.mm
@@ -0,0 +1,77 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="JS ES 6/7" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1464900573662">
+<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Ubuntu" SIZE="8" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+<stylenode TEXT="Monospaced" COLOR="#000000" BACKGROUND_COLOR="#efefef" STYLE="as_parent" MAX_WIDTH="600">
+<font NAME="Ubuntu Mono" SIZE="8" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#333333" BACKGROUND_COLOR="#d6d6ff" STYLE="bubble">
+<font SIZE="9" BOLD="true"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#666666" STYLE="fork">
+<font SIZE="8" BOLD="true"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#0033ff">
+<font SIZE="8"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#009900">
+<font SIZE="8"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="8"/>
+<edge STYLE="sharp_bezier" WIDTH="thin"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="ES 6 new features" POSITION="right" ID="ID_1433013808" CREATED="1464896420406" MODIFIED="1464900587623">
+<edge COLOR="#7c007c"/>
+<node TEXT="Classes" ID="ID_1792454009" CREATED="1464896428943" MODIFIED="1464897085273"/>
+<node TEXT="Arrow Functions" ID="ID_190948872" CREATED="1464897085746" MODIFIED="1464897090689">
+<node TEXT="preserves &apos;this&apos; context" ID="ID_1688020445" CREATED="1464900177659" MODIFIED="1464900198603"/>
+</node>
+<node TEXT="Rest Parameters" ID="ID_1974195651" CREATED="1464897101026" MODIFIED="1464897105746"/>
+<node TEXT="Iterators" ID="ID_1806013962" CREATED="1464897111179" MODIFIED="1464897114450"/>
+<node TEXT="Generators" ID="ID_1453686428" CREATED="1464897114881" MODIFIED="1464897118202"/>
+<node TEXT="Template Strings" ID="ID_903157528" CREATED="1464900419443" MODIFIED="1464900425786"/>
+<node TEXT="Destructuring" ID="ID_948349976" CREATED="1464900452168" MODIFIED="1464900459115">
+<node TEXT="{ attr, func } = object" STYLE_REF="Monospaced" ID="ID_793670726" CREATED="1464900505476" MODIFIED="1464900553510"/>
+</node>
+</node>
+</node>
+</map>
